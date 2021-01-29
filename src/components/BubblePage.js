f29e92a3b96a86da865b256d3f9a568a502e3e40
Jamie <jamielynjenks@gmail.com>
2021-01-29 09:25:33 -0700

    created axiosWithAuth, and Private route, and also fixed the other things wrong so the login page would render

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import {axiosWithAuth} from '../helpers/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(()=>{
+    axiosWithAuth().get(`http://localhost:5000/api/colors`)
+    .then(res=>{
+      setColorList(res.data)
+    })
+    .catch(drama=>{
+      console.log("There was an error with your request")
+      console.log(drama)
+    })
+  },[])
 
   return (
     <>
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,5 +1,17 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
 
+function PrivateRoute ({ component: Component}){
+    const history =  useHistory()
 
+    if(window.localStorage.getItem('token')){
+        return(<Component/>)
+    }
+    else{
+        history.push('/')
+    }
+}
+export default PrivateRoute
 
 
 
